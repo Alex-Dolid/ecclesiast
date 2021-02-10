aa8c977b66809bb76a7b1f9a6a6e9ec8038b9807
Alex Dolid <dolsd.sasha@mail.ru>
2021-02-10 16:10:37 +0200

    [setuped] the project

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: true
+  },
+
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended"
+  ],
+
+  globals: {
+    _: true
+  },
+
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+
+  // add your custom rules here
+  rules: {
+    "no-extra-boolean-cast": 0,
+    "no-debugger": 0,
+    "object-curly-spacing": ["error", "always"],
+    "quotes": [
+      "error",
+      "double"
+    ]
+  },
+
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+      ],
+      env: {
+        jest: true
+      }
+    },
+  ]
+};

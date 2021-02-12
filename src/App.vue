3b59e40308b68c29b0f3f794aff65e72c1bf95f4
Alex Dolid <dolsd.sasha@mail.ru>
2021-02-12 22:45:38 +0200

    [added] styles and main form layout

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="page-width app">
+    <h1>Еклезіаст</h1>
+    <form class="main-form" @submit.prevent="submit">
+      <div class="main-form__search-field">
+        <label for="search-field" class="main-form__search-field-label" />
+        <input
+          id="search-field"
+          class="main-form__search-field-input"
+          type="search"
+          placeholder="Ведіть значення..."
+          v-model="searchValue"
+        />
+      </div>
+      <button class="main-form__search-btn" type="submit">Пошук</button>
+    </form>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "App",
-  components: {
-    HelloWorld
+
+  setup () {
+    const searchValue = ref("");
+    const submit = () => console.log(searchValue.value);
+
+    return {
+      searchValue,
+      submit
+    }
   }
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="scss">
+  .app {
+    text-align: center;
+    height: 100%;
+  }
+
+  .main-form {
+    display: flex;
+    justify-content: center;
+
+    &__search-field {
+      width: 30%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+
+    &__search-field-input {
+      padding: 5px 7px;
+    }
+
+    &__search-btn {
+      margin-left: 10px;
+      width: 100px;
+      padding: 5px 0;
+    }
+  }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 // Core
 import { createApp } from "vue"
+// App
 import App from "./App.vue"
+// Styles
+import "../styles/index.scss";
 
 createApp(App).mount("#app");

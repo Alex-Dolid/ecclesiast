5400fb47efd3955aed0333a2b27a94f0e90453be
Alex Dolid <dolsd.sasha@mail.ru>
2021-02-14 15:27:08 +0200

    [added] logic of api

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1>Еклезіаст</h1>
     <form class="main-form" @submit.prevent="submit">
       <div class="main-form__search-field">
-        <label for="search-field" class="main-form__search-field-label" />
+        <label for="search-field" class="main-form__search-field-label"/>
         <input
           id="search-field"
           class="main-form__search-field-input"
@@ -14,25 +14,56 @@
       </div>
       <button class="main-form__search-btn" type="submit">Пошук</button>
     </form>
+    <div class="cards-draft-bible">
+      <div class="divider"/>
+      <div class="card-draft-bible" v-for="draft of bibleDrafts" :key="draft.lank">
+        <h4 class="card-draft-bible__title">
+          <a
+            :href="draft.links['jw.org']"
+            class="card-draft-bible__title-link"
+            v-html="draft.title"
+          />
+        </h4>
+        <p
+          class="card-draft-bible__text"
+          v-html="draft.snippet"
+        />
+      </div>
+    </div>
+
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+  // Core
+  import { defineComponent, ref } from "vue";
+  // Api
+  import api from "@/api";
 
-export default defineComponent({
-  name: "App",
+  export default defineComponent({
+    name: "App",
 
-  setup () {
-    const searchValue = ref("");
-    const submit = () => console.log(searchValue.value);
+    setup() {
+      const searchValue = ref("");
+      let bibleDrafts = ref([]);
+      const getBibleDrafts = async () => await api.get<any>("/search/results/U/bible", {
+        params: {
+          sort: "rel",
+          q: searchValue.value
+        }
+      });
+      const submit = async () => {
+        const { data: { results } } = await getBibleDrafts();
+        bibleDrafts.value = results;
+      };
 
-    return {
-      searchValue,
-      submit
+      return {
+        searchValue,
+        bibleDrafts,
+        submit
+      }
     }
-  }
-});
+  });
 </script>
 
 <style scoped lang="scss">
@@ -60,6 +91,24 @@ export default defineComponent({
       margin-left: 10px;
       width: 100px;
       padding: 5px 0;
+    }
+  }
+
+  .cards-draft-bible {
+    padding: 0 10%;
+  }
+
+  .card-draft-bible {
+    text-align: start;
+
+    &__title {
+      margin-bottom: 0;
+    }
+
+    &__title-link {
+      &:hover {
+        text-decoration: underline;
+      }
     }
   }
 </style>

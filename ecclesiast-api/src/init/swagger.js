b619af926025cc9a1a18830fe1970d4aa99db94b
captify-alex-dolid <alex.dolid@captify.co.uk>
2021-10-04 00:08:21 +0300

    [added] some swagger options

--- ecclesiast-api/src/init/swagger.js
+++ ecclesiast-api/src/init/swagger.js
@@ -2,12 +2,18 @@ module.exports = {
   info: {
     version: "1.0.0",
     title: "Ecclesiast API",
-    description: "ecclesiast-api"
+    description: "This Api for Ecclesiast projects",
+    contact: {
+      name: "Alex Dolid", // your name
+      email: "admin@admin.com", // your email
+      url: "web.com" // your website
+    }
   },
   openapi: "3.0.0",
   servers: [
     {
-      url: "http://localhost:3030"
+      url: "http://localhost:3030",
+      description: "Local server"
     }
   ],
   apis: [ "src/modules/**/sw.yaml", "src/modules/**/index.**" ]
